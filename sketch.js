6d494fa12244bfb4120114bf92a0ff37394824d1
Arush1234 <80717916+Arush1234@users.noreply.github.com>
2021-11-01 20:52:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,11 +22,9 @@ rabbit.addImage(rabbitImg);
 function draw() {
   background(0);
   rabbit.x = World.mouseX;
-  
   edges= createEdgeSprites();
   rabbit.collide(edges);
-  
-   drawSprites();
+  drawSprites();
   
   var select_sprites = Math.round(random(1,3));
   
@@ -48,7 +46,6 @@ apple.addImage(appleImg);
 apple.scale=0.07;
 apple.velocityY = 3;
 apple.lifetime = 150;
-  
 }
 
 function createOrange() {
